1b8f07b3b61b1fab88461802bbad9d0b6b587945
qingfuw <sbs_sdet_w@hotmail.com>
2014-05-22 20:35:39 +0800

    add fctesting

---
+++ FCtesting/Scripts/fctesting.ps1
@@ -0,0 +1,30 @@
+﻿$linuxagentpath=$args[0]
+$distro=$args[1]
+
+echo "linuxagentpath  $linuxagentpath distro $distro "
+cd C:\DeploymentScripts_FC123_withPdu
+
+ $nodesinfo = .\StartFCClient.cmd nodes?;
+ $str_nodesinfo=[System.String]::Join(";",$nodesinfo);
+ $str_nodesinfo  -match '(?m).*(00000000-\S*).*'
+ $nodeid=$Matches[1]
+ $waitReady=100;
+
+ while($waitReady-- -gt 0)
+{
+  .\StartFCClient.cmd createuseraccount:$nodeid /username:rdTestUser /password:rdPaSSw0rd!! /expiration:30000 
+  if ($LASTEXITCODE -eq 0) {break};
+  sleep 30;
+  write-host "$(date) Wait for $nodeid ready"
+  
+};
+
+sc start RemoteAccess
+
+cd $linuxagentpath
+$tests = "
+OSBlobName,ISOBlobName,TestName,TenantName
+$distro,linux_Certs_page.iso,PositiveTests,$distro.replace(".","")
+"
+Set-Content -Value $tests .\temp_data.csv
+.\RunTestsWithCSV.ps1 .\data2.csv
