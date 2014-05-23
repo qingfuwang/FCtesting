f32bc6acbb7a603f898740068acd61223ccfd4b1
qingfuw <sbs_sdet_w@hotmail.com>
2014-05-23 10:01:15 +0800

    m

--- FCtesting/Scripts/fctesting.ps1
+++ FCtesting/Scripts/fctesting.ps1
@@ -9,26 +9,23 @@ cd C:\DeploymentScripts_FC123_withPdu
  $str_nodesinfo  -match '(?m).*(00000000-\S*).*'
  $nodeid=$Matches[1]
  $waitReady=100;
+
 #$nodeid="00000000-0000-0000-0000-008cfa086314"
  $output = .\StartFCClient.cmd nodeip? /node:$nodeid
  $str_nodesip=[System.String]::Join(";", $output);
  $str_nodesip  -match '(?m).*'+$nodeid+' - (.*);; .*'
 $nodeip=$Matches[1]
 
- net use /DELETE \\$nodeip
- "net use \\$nodeip /user:rdTestUser $password"
- net use \\$nodeip /user:rdTestUser $password
- if(-not $LASTEXITCODE -eq 0)
-{
  while($waitReady-- -gt 0)
 {
+  .\StartFCClient.cmd deleteuseraccount:$nodeid /username:rdTestUser 
   .\StartFCClient.cmd createuseraccount:$nodeid /username:rdTestUser /password:$password /expiration:30000 
   if ($LASTEXITCODE -eq 0) {break};
   sleep 30;
   write-host "$(date) Wait for $nodeid ready"
   
 };
-}
+
 
 sc start RemoteAccess
 
